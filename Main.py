a4d5eb84d617a924e2c2e7ff7f8aa52148ec4a86
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2024-10-22 21:15:33 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from tkinter.ttk import *
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 
 co0 = "#f0f3f5" 
 co1 = "#f0f3f5"  
@@ -12,10 +13,12 @@ co6 = "#ef5350"
 co7 = "#93cd95"  
 
 janela = Tk()
-janela.title("Nibba Software")
-janela.geometry('500x450')
+janela.title("Software Inclusão de idosos")
+largura_tela = janela.winfo_screenwidth()
+altura_tela = janela.winfo_screenheight()
+janela.geometry("500x450")
 janela.configure(background=co2)
-janela.resizable(width=FALSE,height=FALSE)
+janela.resizable(width=TRUE,height=TRUE)
 
 style = Style(janela)
 style.theme_use("clam")
@@ -31,14 +34,16 @@ frame_tabela.grid(row=2,column=0,columnspan=2,padx=10,pady=1,sticky=NW)
 
 #configurando a parte de cima
 
-l_nome = Label(frame_cima,text='Agenta Telefónica',anchor=NE,font=('arial 20 bold'),bg=co3,fg=co1)
+l_nome = Label(frame_cima,text='Agenda Telefônica',font=('arial 20 bold'),bg=co3,fg=co1)
+l_nome.pack(fill=BOTH,expand=TRUE)
 l_nome.place(x=5,y=5)
+
 
 l_linha = Label(frame_cima,text='',width=500,anchor=NE,font=('arial 1'),bg=co2,fg=co1)
 l_linha.place(x=0,y=46)
 
 #configurando o formulário
-l_nome = Label(frame_baixo,text='Nome *',anchor=NW,font=('ivy 10'),bg=co1,fg=co4)
+l_nome = Label(frame_baixo,text='Nome *',anchor=NW,font=('ivy 15'),bg=co1,fg=co4)
 l_nome.place(x=10,y=20)
 en_nome = Entry(frame_baixo,width=25,justify='left',font=('',10),highlightthickness=1)
 en_nome.place(x=80,y=20)
@@ -47,7 +52,7 @@ en_nome.place(x=80,y=20)
 l_sexo = Label(frame_baixo,text='Sexo *',anchor=NW,font=('ivy 10'),bg=co1,fg=co4)
 l_sexo.place(x=10,y=50)
 c_sexo = Combobox(frame_baixo,width=27)
-c_sexo['value'] = ('','F','M')
+c_sexo['value'] = ('','F','M','gambiarra do kpta')
 c_sexo.place(x=80,y=50)
 
 l_Telefone = Label(frame_baixo,text='Telefone*',anchor=NW,font=('ivy 10'),bg=co1,fg=co4)
@@ -57,13 +62,12 @@ en_Telefone.place(x=80,y=80)
 
 l_Email = Label(frame_baixo,text='E-mail*',anchor=NW,font=('ivy 10'),bg=co1,fg=co4)
 l_Email.place(x=10,y=110)
-en_Email = Entry(frame_baixo,width=25,justify='left',font=('',10),highlightthickness=1)
+en_Email = Entry(frame_baixo,width=25,justify='left',relief="sunken",font=('',10),highlightthickness=1)
 en_Email.place(x=80,y=110)
 
 b_procurar = Button(frame_baixo,text='procurar',font=('ivy 8 bold'),bg=co1,fg=co4,relief=RAISED,overrelief=RIDGE)
 b_procurar.place(x=290,y=20)
 en_procurar = Entry(frame_baixo,width=16,justify='left',font=('',11),highlightthickness=1)
-en_Email.place(x=347,y=20)
 
 b_Olhar = Button(frame_baixo,text='Ver dados',width=10,font=('ivy 8 bold'),bg=co1,fg=co4,relief=RAISED,overrelief=RIDGE)
 b_Olhar.place(x=290,y=50)
@@ -76,5 +80,27 @@ b_Atualizar.place(x=400,y=80)
 
 b_Deletar = Button(frame_baixo,text='Deletar',width=10,font=('ivy 8 bold'),bg=co1,fg=co4,relief=RAISED,overrelief=RIDGE)
 b_Deletar.place(x=400,y=110)
+
+#configurando frame tabela
+Cab_lista = ['Nome','Sexo','Telefone','e-mail']
+dados = [['joao','M','123456','joaomigga@'],
+         ['julia','F','123456','giganibba']]
+
+tree = ttk.Treeview(frame_tabela,selectmode="extended",columns=Cab_lista,show="headings")
+
+#barra de rolagem
+vbr = ttk.Scrollbar(frame_tabela,orient="vertical",command=tree.yview)
+hbr = ttk.Scrollbar(frame_tabela,orient="horizontal",command=tree.xview)
+
+tree.configure(yscrollcommand=vbr.set,xscrollcommand=hbr.set)
+
+tree.grid(column=0,row=0,sticky='nsew')
+vbr.grid(column=1,row=0,sticky='ns')
+hbr.grid(column=0,row=1,sticky='ew')
+
+hd = ["nw","nw","nw","nw","nw"]
+h=[120,50,80,120,200]
+n=0
+
 
 janela.mainloop()
